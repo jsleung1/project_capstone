6ecdcbe09dbe896a046c099757b634b9d037be78
jsleung1 <jsleung1@yahoo.com>
2019-12-22 12:42:13 +0800

    test commit

--- client/src/app/config.ts
+++ client/src/app/config.ts
@@ -1,4 +1,4 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it.
 const apiId = 'lh756iw3bi';
 const env = 'v2';
 
@@ -15,4 +15,3 @@ export const authConfig_dev = {
   clientId: 'Z52r8N5nV8h3XvWr3jqZJm9wE3JWi6yq',
   callbackUrl: 'http://localhost:4200/school/auth0'
 }
-
