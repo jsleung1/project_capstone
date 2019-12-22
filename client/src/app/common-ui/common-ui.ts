857699888f0aeaa525ddc40c618e3e1aae335cfe
jsleung1 <jsleung1@yahoo.com>
2019-12-22 11:26:11 +0800

    rename course module

--- client/src/app/common-ui/common-ui.ts
+++ client/src/app/common-ui/common-ui.ts
@@ -1,4 +1,4 @@
 export * from './dialog/alert-dialog/alert-dialog.component';
-export * from '../veriguide-course/course-info/course-info.component';
+export * from '../course/course-info/course-info.component';
 export * from '../veriguide-main/main-menu/main-menu.component';
 
