b209c01c1c3127a5bea618949842eaf71f2fd662
jsleung1 <jsleung1@yahoo.com>
2019-12-22 12:36:36 +0800

    update config before prod deployment

--- client/src/app/config.ts
+++ client/src/app/config.ts
@@ -4,13 +4,13 @@ const env = 'v2';
 
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/${env}`
 
-export const authConfig_prod = {
+export const authConfig = {
   domain: 'dev-clq116aa.auth0.com',
   clientId: 'QLaMP23u6l9whbZWmXf6PTF9eKxMSVU8',
   callbackUrl: 'https://verimarker.com/school/auth0'
 }
 
-export const authConfig = {
+export const authConfig_dev = {
   domain: 'dev-clq116aa.auth0.com',
   clientId: 'Z52r8N5nV8h3XvWr3jqZJm9wE3JWi6yq',
   callbackUrl: 'http://localhost:4200/school/auth0'
