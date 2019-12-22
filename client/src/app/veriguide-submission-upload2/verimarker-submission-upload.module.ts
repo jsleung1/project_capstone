3fa2c209d2cb6db3896e95ca0bf5cf5affb14b30
jsleung1 <jsleung1@yahoo.com>
2019-12-22 12:01:03 +0800

    update module

--- client/src/app/veriguide-submission-upload2/verimarker-submission-upload.module.ts
+++ client/src/app/veriguide-submission-upload2/verimarker-submission-upload.module.ts
@@ -2,7 +2,7 @@ import { AssignmentUploadComponent } from './assignment-upload/assignment-upload
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { VerimarkerSubmissionUpload2RoutingModule } from './veriguide-submission-upload2-routing.module';
+import { VerimarkerSubmissionUpload2RoutingModule } from './verimarker-submission-upload-routing.module';
 import { VerimarkerCommonUiModule } from '../common-ui/verimarker-common-ui.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
